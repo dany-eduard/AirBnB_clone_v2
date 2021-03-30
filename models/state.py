67e7b7f3c885c9d1ea0ddaa34ede6f758a1e4170
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-30 03:18:23 +0000

    Revert clases

--- models/state.py
+++ models/state.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
-""" States Module for HBNB project"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.city import City
-import models
-import os
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
-    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            city = []
-            for obj in list(models.storage.all(City).values()):
-                if obj.state_id == self.id:
-                    city.append(obj)
-            return city
+    name = ""
