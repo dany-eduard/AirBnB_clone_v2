64b98438f20c581801ecf35f0263d0cf7258e590
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-05-01 03:48:23 +0000

    Script that starts a Flask web app

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello Flask! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Print C<text> """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Print python texts, default ('is cool'). """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def number(num):
+    """ It's a number! """
+    if type(num) == int:
+        return ("{} is a number".format(num))
+
+
+@app.route('/number_template/<int:num>', strict_slashes=False)
+def display_html(num):
+    """ Display a HTML page. """
+    if type(num) == int:
+        return render_template('5-number.html', n=num)
+
+
+@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
+def odd_or_even(num):
+    """ Odd or even? """
+    if type(num) == int:
+        return render_template('6-number_odd_or_even.html', n=num)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
